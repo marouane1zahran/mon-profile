279dce3d4c77f719f4953b1b6a41a7616ee67d26
marouane <zahranmarouane77@gmail.com>
2026-03-24 00:16:12 +0100

    feat: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand': {
+          'primary': '#2563eb',
+          'secondary': '#1e40af',
+        }
+      },
+    },
+  },
+  plugins: [],
+}
